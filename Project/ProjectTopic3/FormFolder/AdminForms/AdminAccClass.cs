c9594efcc518ed04b8d22278bb7ec3f2830d4cef
tuyetphale993 <tuyetphale993@gmail.com>
2015-06-15 19:42:49 +0700

    - Add entity framework - Add some layouts
    Signed-off-by: tuyetphale993 <tuyetphale993@gmail.com>

---
+++ Project/ProjectTopic3/FormFolder/AdminForms/AdminAccClass.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Project_Topic3.FormFolder.AdminForms
+{
+    public partial class AdminAccClass : AbstractForms.AccessObj
+    {
+        public AdminAccClass()
+        {
+            InitializeComponent();
+            this.lbTitleLayout.Text = "QUẢN LÝ LỚP HỌC";
+            this.lbTittleDataGridview.Text = "Danh sách lớp học";
+        }
+    }
+}
---
+++ Project/ProjectTopic3/FormFolder/AdminForms/AdminAccTimetable.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Project_Topic3.FormFolder.AdminForms
+{
+    public partial class AdminAccTimetable : AbstractForms.AccessObj
+    {
+        public AdminAccTimetable()
+        {
+            InitializeComponent();
+            this.lbTitleLayout.Text = "QUẢN LÝ THỜI KHÓA BIỂU";
+            this.lbTittleDataGridview.Text = "Danh sách thời khóa biểu";
+        }
+    }
+}
