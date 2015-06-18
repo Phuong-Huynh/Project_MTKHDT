e25fba3161a8c03393c4e5b1f390d83b12d19667
tuyetphale993 <tuyetphale993@gmail.com>
2015-06-18 23:43:28 +0700

    update login layout
    Signed-off-by: tuyetphale993 <tuyetphale993@gmail.com>

--- Project/ProjectTopic3/FormFolder/Login.Designer.cs
+++ Project/ProjectTopic3/FormFolder/Login.Designer.cs
@@ -102,17 +102,17 @@
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 6;
-            this.button1.Text = "button1";
+            this.button1.Text = "Admin";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(137, 248);
+            this.button2.Location = new System.Drawing.Point(160, 248);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(75, 23);
             this.button2.TabIndex = 7;
-            this.button2.Text = "button2";
+            this.button2.Text = "Student";
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
@@ -122,7 +122,7 @@
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(75, 23);
             this.button3.TabIndex = 8;
-            this.button3.Text = "button3";
+            this.button3.Text = "Teacher";
             this.button3.UseVisualStyleBackColor = true;
             this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
